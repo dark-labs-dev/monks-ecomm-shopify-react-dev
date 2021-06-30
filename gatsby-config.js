a464fca6b989868e8172f8a50fc6779fc18dbe7f
future-rob <rob@dark-labs.co>
2021-06-30 08:57:33 -0700

    update favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,7 +31,7 @@ module.exports = {
         background_color: `#d6cf8c`,
         theme_color: `#d6cf8c`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/monks-m.png`, // This path is relative to the root of the site.
       },
     },
     {
