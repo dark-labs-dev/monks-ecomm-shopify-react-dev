f138581497be3073613fa367a879748bb4173c34
future-rob <rob@dark-labs.co>
2021-07-12 17:45:44 -0700

    monks shopify dev init

--- src/components/HeroImage.js
+++ src/components/HeroImage.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import HeroImage0 from '../images/Meditation_Hero.jpeg'
+import HeroImage0 from '../images/Meditation_HERO.jpeg'
 import HeroImage1 from '../images/Window_Hero.jpeg'
 import "./styles.css"
 
@@ -7,7 +7,7 @@ export default function HeroImage() {
     return (
         <div className="hero--wrapper">
             <img className="hero--image" src={HeroImage0} />
-            <img className="hero--image" src={HeroImage1} />
+            {/* <img className="hero--image" src={HeroImage1} /> */}
         </div>
     )
 }
